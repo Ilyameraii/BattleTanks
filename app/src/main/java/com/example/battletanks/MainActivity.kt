60f044100c13cd886b07379f56486e488b5ee3c3
Ilyameraii <ilia.bobrow2006@gmail.com>
2025-05-06 14:34:29 +0300

    commit

--- app/src/main/java/com/example/battletanks/MainActivity.kt
+++ app/src/main/java/com/example/battletanks/MainActivity.kt
@@ -1,20 +1,52 @@
 package com.example.battletanks
 
 import android.os.Bundle
+import android.view.KeyEvent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.view.KeyEvent.KEYCODE_DPAD_DOWN
+import android.view.KeyEvent.KEYCODE_DPAD_LEFT
+import android.view.KeyEvent.KEYCODE_DPAD_RIGHT
+import android.view.KeyEvent.KEYCODE_DPAD_UP
+import com.example.battletanks.Direction.UP
+import com.example.battletanks.Direction.DOWN
+import com.example.battletanks.Direction.LEFT
+import com.example.battletanks.Direction.RIGHT
+import com.example.battletanks.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        when(keyCode){
+            KEYCODE_DPAD_UP -> move(UP)
+            KEYCODE_DPAD_DOWN -> move(DOWN)
+            KEYCODE_DPAD_LEFT -> move(LEFT)
+            KEYCODE_DPAD_RIGHT -> move(RIGHT)
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+    private fun move(direction:Direction){
+        when(direction){
+            UP->{
+
+            }
+            DOWN->{
+
+            }
+            LEFT->{
+
+            }
+            RIGHT->{
+
+            }
         }
     }
 }
